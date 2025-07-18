5d810d748127ae1cd76c597eb4fdca3a2cd8589d
eycis <eycisxxx@gmail.com>
2025-07-18 19:51:55 +0200

    image debug

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -24,7 +24,7 @@ const MainPage = () => {
         }
     };
     fetchData();
-  }, []);
+  },[]);
 
   const getRandomFriend = async() => {
     try{
--- config/databaseAdmin.ts
+++ config/databaseAdmin.ts
@@ -1,27 +1,11 @@
-import * as admin from "firebase-admin";
-//import serviceAccount from "@/config/serviceAccountKey.json";
+// config/databaseAdmin.ts
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
 
-//production ver:
 if (!admin.apps.length) {
-  try {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  } catch (error) {
-    console.error("error with firebase service account:", error);
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
 }
 
 export const dbAdmin = admin.firestore();
-
-//uncomment if on dev:
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-//   });
-// }
-
-// const dbAdmin = admin.firestore();
-
-// export { dbAdmin };
