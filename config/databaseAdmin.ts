88c9a6cf0f05bdeefa071748a30c1570cbc9e6a0
eycis <eycisxxx@gmail.com>
2025-07-18 19:33:41 +0200

    databaseAdmin debug

--- config/databaseAdmin.ts
+++ config/databaseAdmin.ts
@@ -1,11 +1,27 @@
-// config/databaseAdmin.ts
-import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";
+import * as admin from "firebase-admin";
+//import serviceAccount from "@/config/serviceAccountKey.json";
 
+//production ver:
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  });
+  try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } catch (error) {
+    console.error("error with firebase service account:", error);
+  }
 }
 
 export const dbAdmin = admin.firestore();
+
+//uncomment if on dev:
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//   });
+// }
+
+// const dbAdmin = admin.firestore();
+
+// export { dbAdmin };
